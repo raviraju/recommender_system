b86c7d0ec557e1fb35aa2ea0861624f8f0428559
Ravi Raju Krishna <ravirajuk.raju@gmail.com>
2017-10-17 12:00:20 -0700

    precision recall for books recommender

--- books_recommender/popularity_based.py
+++ books_recommender/popularity_based.py
@@ -7,6 +7,7 @@ from sklearn.cross_validation import train_test_split
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from recommender.popularity_based import PopularityBasedRecommender
+from recommender.evaluation import PrecisionRecall
 
 def load_train_test(learner_books_file):
     """Loads data and returns training and test set"""
@@ -29,15 +30,19 @@ def main():
     train_data, test_data = load_train_test(learner_books_file)
     #print(train_data.head(5))
     #print(test_data.head(5))
-    popularity_reco = PopularityBasedRecommender(results_dir)
-    popularity_reco.train(train_data, 'learner_id', 'book_code')
+    model = PopularityBasedRecommender(results_dir)
+    model.train(train_data, 'learner_id', 'book_code')
 
     learners = test_data['learner_id'].unique()
     learner_id = learners[0]
     #learner_id = 5
-    recommendations = popularity_reco.recommend(learner_id)
-    #print(recommendations)
-    print("Popularity Based Recommendations are found in results/")
+    recommended_items = model.recommend(learner_id)
+    print(recommended_items)
+    #print("Popularity Based Recommendations are found in results/")
+
+    precision_recall_intf = PrecisionRecall(train_data, test_data, model, user_id_col='user_id', item_id_col='song')
+    results = precision_recall_intf.compute_measures(test_users_percentage=0.1)
+    print(results)
 
 if __name__ == '__main__':
     main()
