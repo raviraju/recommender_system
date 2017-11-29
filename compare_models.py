27fddc509e31f28d3847e65f48fd11844bb73bd8
Ravi Raju Krishna <ravirajuk.raju@gmail.com>
2017-11-29 08:59:06 +0000

    compare models using precision, recall, f1_score and auc curve

--- compare_models.py
+++ compare_models.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 from pathlib import Path
 from pprint import pprint
 from lib import utilities
@@ -8,6 +9,56 @@ matplotlib.use('agg')
 import matplotlib.pyplot as plt
 from collections import OrderedDict
 
+def plot_auc(recommender, results):
+    #pprint(results)
+    
+    colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k']#http://matplotlib.org/1.3.1/api/pyplot_api.html#matplotlib.pyplot.plot
+    no_of_items_to_recommend = []
+    avg_precision_models_dict = OrderedDict()
+    avg_recall_models_dict = OrderedDict()
+    for no_of_items in results:
+        no_of_items_to_recommend.append(int(no_of_items))
+        for model in results[no_of_items]:
+            if model not in avg_precision_models_dict:
+                avg_precision_models_dict[model] = []
+            if model not in avg_recall_models_dict:
+                avg_recall_models_dict[model] = []
+    #print(no_of_items_to_recommend)
+    for no_of_items in no_of_items_to_recommend:
+        for model in avg_precision_models_dict.keys():
+            avg_precision_val = 1 - float(results[str(no_of_items)][model]['avg_precision'])           
+            avg_precision_models_dict[model].append(avg_precision_val)
+        for model in avg_recall_models_dict.keys():
+            avg_recall_val = float(results[str(no_of_items)][model]['avg_recall'])           
+            avg_recall_models_dict[model].append(avg_recall_val)            
+    #pprint(avg_precision_models_dict)
+    #pprint(avg_recall_models_dict)
+
+    fig = plt.figure(figsize=(8,6))
+    ax1 = fig.add_subplot(111)
+
+    i = 0
+    for model in avg_precision_models_dict:
+        precisions = avg_precision_models_dict[model]
+        recalls = avg_recall_models_dict[model]
+       
+        x_precisions = np.array(precisions)
+        y_recalls = np.array(recalls)
+        
+        ax1.plot(x_precisions, y_recalls, label=model, color=colors[i], marker='o')
+        i = i+1
+    plt.ylabel('recall')
+    plt.xlabel('1-precision')
+        
+    handles, labels = ax1.get_legend_handles_labels()
+    lgd = ax1.legend(handles, labels, loc='upper center', bbox_to_anchor=(0.8,1))
+    ax1.grid('on')
+    img_name = 'results_auc.png'
+    img_file = os.path.join(recommender, 'results', img_name)
+    print("Generated Plot : {}".format(img_file))
+    plt.savefig(img_file)
+    plt.show()
+    
 def plot_graph(recommender, results, measure):
     #pprint(results)
     
@@ -46,6 +97,7 @@ def plot_graph(recommender, results, measure):
     ax1.grid('on')
     img_name = 'results_' + measure + '.png'
     img_file = os.path.join(recommender, 'results', img_name)
+    print("Generated Plot : {}".format(img_file))
     plt.savefig(img_file)
     plt.show()
 
@@ -66,6 +118,14 @@ def analyse(recommender):
     plot_graph(recommender, results, 'avg_f1_score')
     plot_graph(recommender, results, 'avg_precision')
     plot_graph(recommender, results, 'avg_recall')
+    plot_auc(recommender, results)
 
 if __name__ == '__main__':
-    analyse('books_recommender/')
+    parser = argparse.ArgumentParser(description="Compare Recommender Models")
+    parser.add_argument("model", help="Use Case Recommender Model Directory")
+    args = parser.parse_args()
+    if args.model:
+        print("Analysing results of ", args.model)
+        analyse(args.model)
+    else:
+        print("Specify use case recommender model directory to analyse")
